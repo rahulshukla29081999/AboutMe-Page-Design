7ecc6c6897784157db9a223a23ac86a54d6b13e1
Rahul Shukla <47075543+rahulshukla29081999@users.noreply.github.com>
2026-01-07 02:34:12 +0530

    Updated portfolio UI and features

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleTheme() {
+  document.body.classList.toggle("dark");
+  localStorage.setItem(
+    "theme",
+    document.body.classList.contains("dark") ? "dark" : "light"
+  );
+}
+
+window.onload = () => {
+  if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+  }
+};
